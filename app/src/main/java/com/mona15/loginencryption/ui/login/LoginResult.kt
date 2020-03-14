3a0ec6862cab26a66b10b1463c6c5b62bb394ecd
Zorayda <zgutierrez@americanassist.com>
2020-03-14 13:13:51 -0500

    Put format archive witch java

--- app/src/main/java/com/mona15/loginencryption/ui/login/LoginResult.kt
+++
@@ -1,9 +0,0 @@
-package com.mona15.loginencryption.ui.login
-
-/**
- * Authentication result : success (user details) or error message.
- */
-data class LoginResult (
-     val success:LoggedInUserView? = null,
-     val error:Int? = null
-)
